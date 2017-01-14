89779520e1b21ebab0ba54c3f74c109f0b6f06ff
Carlos Rivero Aro <carlosriveroaro7@gmail.com>
2017-01-14 15:25:06 +0100

    Create: funcional, necesita base de datos en heroku

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 
 	r "gopkg.in/gorethink/gorethink.v2"
+	"strconv"
 )
 
 var port = os.Getenv("PORT")
@@ -20,15 +21,15 @@ type CRUD interface {
 }
 
 type UsuarioHotel struct {
-	ID                 uint   `json:"id"`
-	Nombre             string `json:"nombre"`
-	Apellido           string `json:"apellido"`
-	Numero             int    `json:"numero"`
-	PaisDeOrigen       string `json:"pais_de_origen"`
-	NumeroDeHabitacion int    `json:"numero_de_habitacion"`
+	ID                 uint   `gorethink:"id"`
+	Nombre             string `gorethink:"nombre"`
+	Apellido           string `gorethink:"apellido"`
+	Numero             int    `gorethink:"numero"`
+	PaisDeOrigen       string `gorethink:"pais_de_origen"`
+	NumeroDeHabitacion int    `gorethink:"numero_de_habitacion"`
 }
 
-func session() (*r.Session) {
+func session() *r.Session {
 	session, err := r.Connect(r.ConnectOpts{
 		Address: "localhost:28015",
 	})
@@ -67,33 +68,113 @@ func Read(id uint) (interface{}, error) {
 	return nUser, nil
 }
 
-func index(response http.ResponseWriter, request *http.Request) {
-	tpl, err := template.New("index").Parse(`
-	<html>
-		<head>
-		<title>Pagina de prubas CRUD</title>
-		</head>
+func Update(id uint, data interface{}) error {
+	return nil
+}
 
-		<body>
-			<ul>
-				<li><a href="/create">Create</a></li>
-				<li><a href="/read">Read</a></li>
-				<li><a href="/update">Update</a></li>
-				<li><a href="/delete">Delete</a></li>
-			</ul>
-		</body>
-
-	</html>
-	`)
-	if err != nil {
-		log.Fatalln(err)
+func methodHandler(method string, get, post func()) {
+	switch method {
+	case "GET":
+		get()
+	case "POST":
+		post()
 	}
-	tpl.Execute(response, nil)
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+
+	get := func() {
+		tpl, err := template.New("create").Parse(`
+		<html>
+			<head>
+			<title>Pagina de prubas CRUD</title>
+			</head>
+
+			<body>
+				<button><a href="/">Back</a></button>
+				<div>
+					<form>
+					<input type="text" name="id" placeholder="ID Card Number">
+					<input type="text" name="name" placeholder="Name">
+					<input type="text" name="surname" placeholder="Surname">
+					<input type="tel" name="telnum" placeholder="Telephone Number">
+					<input type="text" name="country" placeholder="Country Of Origin">
+					<input type="number" name="room_number" placeholder="Room Number">
+
+					<button formmethod="post" formaction="/create">Create</button>
+					</form>
+				</div>
+			</body>
+
+		</html>
+		`)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		tpl.Execute(w, nil)
+	}
+	post := func() {
+		stringToInt := func(s string) int {
+			i, err := strconv.Atoi(s)
+			if err != nil {
+				log.Fatal(err)
+			}
+			return i
+		}
+
+		nuevoUsuario := UsuarioHotel{
+			ID:                 uint(stringToInt(r.FormValue("id"))),
+			Nombre:             r.FormValue("name"),
+			Apellido:           r.FormValue("surname"),
+			Numero:             stringToInt(r.FormValue("telnum")),
+			PaisDeOrigen:       r.FormValue("country"),
+			NumeroDeHabitacion: stringToInt(r.FormValue("room_number")),
+		}
+		err := Create(nuevoUsuario)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	methodHandler(r.Method, get, post)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+
+	get := func() {
+		tpl, err := template.New("index").Parse(`
+		<html>
+			<head>
+			<title>Pagina de prubas CRUD</title>
+			</head>
+
+			<body>
+				<ul>
+					<li><a href="/create">Create</a></li>
+					<li><a href="/read">Read</a></li>
+					<li><a href="/update">Update</a></li>
+					<li><a href="/delete">Delete</a></li>
+				</ul>
+			</body>
+
+		</html>
+		`)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		tpl.Execute(w, nil)
+	}
+	post := func() {
+
+	}
+
+	methodHandler(r.Method, get, post)
 }
 
 func main() {
 	// Servidor web
 	http.HandleFunc("/", index)
+	http.HandleFunc("/create", create)
 
 	if port == "" {
 		port = "8080"
